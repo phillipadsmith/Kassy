94113450d874359b7344d17b995e1c314009310d
Matthew <mrk45@uclive.ac.nz>
2016-02-28 23:01:51 +1300

    Perform an NPM update on update

--- core/core_modules/update.js
+++ core/core_modules/update.js
@@ -1,8 +1,25 @@
 var gitpull = require.safe('git-pull'),
-	path = require('path');
+	path = require('path'),
+	npm = require('npm');
 
 exports.match = function(text, commandPrefix) {
 	return text === commandPrefix + 'update';
+};
+
+var npmUpdate = function() {
+	npm.load({loglevel: "silent"}, function(err, data) {
+		if (err) {
+			return api.sendMessage('Updating dependencies failed. Manual intervention is probably required.', event.thread_id);
+		}
+		
+		npm.commands.update(function(err, data) {
+			if (err) {
+				return api.sendMessage('Updating dependencies failed. Manual intervention is probably required.', event.thread_id);
+			}
+			
+			api.sendMessage('Update complete. Restart to load changes.', event.thread_id);
+		});
+	});
 };
 
 exports.run = function(api, event) {
@@ -11,7 +28,8 @@ exports.run = function(api, event) {
 		if (err) {
 			api.sendMessage('Update failed. Manual intervention is probably required.', event.thread_id);
 		} else {
-			api.sendMessage('Update successful. Restart to load changes.', event.thread_id);
+			api.sendMessage('Application update successful. Updating dependencies...', event.thread_id);
+			npmUpdate();
 		}
 	});
 	return false;
